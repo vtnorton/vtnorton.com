e0a1a08b3ff3f5d665b192f4e07d2f775806cef2
Vitor Norton <vtnorton@outlook.com>
2023-06-09 09:35:44 -0300

    Fix code scanning alert - Potentially unsafe external link #20

--- src/components/BookClubeComponent/YoutubeBookClubComponent/BookClubVideoItemComponent.tsx
+++ src/components/BookClubeComponent/YoutubeBookClubComponent/BookClubVideoItemComponent.tsx
@@ -2,7 +2,7 @@ import { PlaylistItem } from '../../../interfaces/YouTube/PlaylistItem'
 
 export const BookClubVideoItemComponent = (props: PlaylistItem) => {
 	return (
-		<a href={props.link} target='_blank' className='video-item'>
+		<a href={props.link} target='_blank' className='video-item' rel='noopener noreferrer'>
 			<h5>🔗 {props.title.replace('[Clube do livro]', '')}</h5>
 		</a>
 	)
