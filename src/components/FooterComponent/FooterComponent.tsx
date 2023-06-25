af5f4fca5a4dd3d8be54168feb4a1a9e02d6ead2
Vitor Norton <vtnorton@outlook.com>
2023-06-25 20:21:25 -0300

    improved secutiry

--- src/components/FooterComponent/FooterComponent.tsx
+++ src/components/FooterComponent/FooterComponent.tsx
@@ -8,7 +8,7 @@ export const FooterComponent = () => {
 		return (
 			<div className='social'>
 				{SocialProfiles.map((item: SocialProfile, index: number) => (
-					<a key={index} href={item.url} target='_blank'>
+					<a key={index} href={item.url} target='_blank' rel='noopener noreferrer'>
 						<FontAwesomeIcon icon={item.icon} />
 					</a>
 				))}
