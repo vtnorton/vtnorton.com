492816ee95e13e4471291ee1208e6697f6239b79
vítor norton <vtnorton@outlook.com>
2023-06-29 11:08:01 -0300

    Fixed RSS issue (#31)

--- src/pages/api/post.tsx
+++ src/pages/api/post.tsx
@@ -3,7 +3,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { BlogGridItemProps } from '../../components'
 import { getBlogSectionItems } from '../../services/notionServices'
-import { generateRssFeed } from '../../services/rssServices'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<BlogGridItemProps[]>) {
 	const cacheKey = 'blogPosts'
@@ -15,7 +14,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 	if (cachedPosts) return res.status(200).json(cachedPosts.slice(0, postQuantity))
 
 	const posts: BlogGridItemProps[] = await getBlogSectionItems(tag, !hideChangelogs)
-	await generateRssFeed(posts)
 	await kv.setex(cacheKey, 60 * 60 * 8, posts)
 
 	return res.status(200).json(posts.slice(0, postQuantity))
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {
 	AthenaPromoComponent,
 	BlogComponent,
+	BlogGridItemProps,
 	BookClubComponent,
 	FooterComponent,
 	ForBusinessComponent,
@@ -16,8 +17,24 @@ import {
 	TwitchComponent,
 } from '../components'
 import { SeoProps } from '../database/SEOProps'
+import { getBlogSectionItems } from '../services/notionServices'
+import { generateRssFeed } from '../services/rssServices'
 
-export default function Index() {
+export const getStaticProps = async (context: any) => {
+	const posts: BlogGridItemProps[] = await getBlogSectionItems()
+	await generateRssFeed(posts)
+
+	let props = {
+		rss: true,
+	}
+	props = JSON.parse(JSON.stringify(props))
+	return {
+		props,
+		revalidate: 60 * 60 * 1,
+	}
+}
+
+export default function Index({ rss }: { rss: boolean }) {
 	return (
 		<>
 			<SeoProps title='vítor norton | developer relations | 🤘🚀🥑' description='Lives na Twitch, clube do livro, desenvolvimento de apps e web, mitologia grega, música e séries de TV. Um compilado do que eu sou e do que eu faço, chega mais!' featureImage='/img/pages/404.jpg' />
