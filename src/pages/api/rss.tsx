dc25e84998e9d8199ab661f88c79e58335b8d0ba
Vitor Norton <vtnorton@outlook.com>
2023-08-10 12:33:08 -0300

    Adjust to notion design

--- src/pages/api/rss.tsx
+++ src/pages/api/rss.tsx
@@ -1,9 +1,10 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next'
 
-import { BlogGridItemProps } from "../../components";
-import { getBlogSectionItems } from "../../services/notionServices";
-import { generateRssFeed } from "../../services/rssServices";
+import { BlogGridItemProps } from '../../components'
+import { getBlogSectionItems } from '../../services/notionServices'
+import { generateRssFeed } from '../../services/rssServices'
 
+//TODO: colocar cache aqui
 export default async (req: NextApiRequest, res: NextApiResponse<string>) => {
 	const posts: BlogGridItemProps[] = await getBlogSectionItems()
 	const rss = await generateRssFeed(posts)
