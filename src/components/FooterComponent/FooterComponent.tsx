ad32e62bd24f8748f4f48db497514a157f1349c0
Deric Parra <parradeko@gmail.com>
2024-03-06 15:11:01 -0300

    fix/ footer component

--- src/components/FooterComponent/FooterComponent.tsx
+++ src/components/FooterComponent/FooterComponent.tsx
@@ -24,13 +24,14 @@ export const FooterComponent = () => {
 					<img src='/img/logo-color.png' alt='vtnorton' />
 				</a>
 				{renderSocialMedia()}
+
 				<p>
 					no downtime for hustle-as-a-service - <a href='mailto:contato@vtnorton.com'>contato@vtnorton.com</a> <br />
 				</p>
 				<p className='creative-commons'>
-					Acesse a <a href='/termos/site'>política de privacidade e termos de uso do site</a>. <br />O conteúdo deste site é licenciado sob a licença{' '}
+					Acesse a <a href='/termos/site'>política de privacidade e termos de uso do site</a>. <br />O conteúdo deste site é licenciado sob a licença:{' '}
 					<a href='http://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1' target='_blank' rel='license noopener noreferrer'>
-						Attribution-NonCommercial-ShareAlike 4.0 (CC BY-NC-SA 4.0) International
+						<br />(CC BY-NC-SA 4.0)<br />
 						<img src='https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1' alt='CC: Creative Commons' />
 						<img src='https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1' alt='BY: Credit must be given to you, the creator.' />
 						<img src='https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1' alt='NC: Only noncommercial use of your work is permitted.' />
