f7fa12bf64822f07754324d185e2d71da6e83062
Vitor Norton <vtnorton@outlook.com>
2024-05-24 19:33:51 -0300

    product shelft

--- src/components/ProductShelfComponet/ProductShelfComponent.tsx
+++ src/components/ProductShelfComponet/ProductShelfComponent.tsx
@@ -1,34 +1,63 @@
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useState } from 'react'
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
-import { productsItems } from '../../database/ProductShelfItems'
-import { Product } from '../../interfaces/Product'
-import { ProductShelfComponentProps } from './ProductShelfComponentProps'
-import { ProductShelfItemComponent } from './ProductShelfItemComponent'
+import { productsItems } from "../../database/ProductShelfItems";
+import { Product } from "../../interfaces/Product";
+import { ProductShelfComponentProps } from "./ProductShelfComponentProps";
+import { ProductShelfItemComponent } from "./ProductShelfItemComponent";
 
 export const ProductShelfComponent = (props: ProductShelfComponentProps) => {
-	const [showUnsupportedProjects, setShowUnsupportedProjects] = useState(false)
+  const [showUnsupportedProjects, setShowUnsupportedProjects] = useState(false);
+  const [projectsToDisplay, setProjectsToDisplay] = useState<Product[]>(
+    productsItems.filter((x) => x.supported),
+  );
 
-	return (
-		<div className='container'>
-			<section className='shalf'>
-				<div className='shalf-explain'>
-					<h2>📑 apps + extensões + projetinhos</h2>
-					<p>Construir algo do nada é o que faz o meu mundo girar. Aqui está todos os projetos que eu já fiz, tô fazendo ou vou fazer ainda.</p>
-					<a onClick={() => setShowUnsupportedProjects(!showUnsupportedProjects)} className='btn btn-link' rel='noopener noreferrer'>
-						<FontAwesomeIcon icon={faChevronRight} />
-						mostrar projetos finalizados
-					</a>
-				</div>
-				<div className='blog-grid shalf-content'>
-					{productsItems
-						.filter((x) => (showUnsupportedProjects ? x.name !== null : x.supported))
-						.map((item: Product, index: number) => {
-							return <ProductShelfItemComponent key={index} name={item.name} link={item.link} imageUrl={item.imageUrl} supported={item.supported} />
-						})}
-				</div>
-			</section>
-		</div>
-	)
-}
+  const updateList = () => {
+    setShowUnsupportedProjects(!showUnsupportedProjects);
+    const products = productsItems.filter((x) =>
+      showUnsupportedProjects ? x.name !== null : x.supported,
+    );
+
+    setProjectsToDisplay(products);
+  };
+
+  return (
+    <div className="container">
+      <section className="shalf">
+        <div className="shalf-explain">
+          <h2>📑 apps + extensões + projetinhos</h2>
+          <p>
+            Construir algo do nada é o que faz o meu mundo girar. Aqui está
+            todos os projetos que eu já fiz, tô fazendo ou vou fazer ainda.
+          </p>
+          <a
+            onClick={() => updateList()}
+            className="btn btn-link"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faChevronRight} />
+            {showUnsupportedProjects
+              ? "mostrar projetos finalizados"
+              : "mostrar apenas projetos suportados"}
+          </a>
+        </div>
+        <div
+          className={`blog-grid shalf-content ${projectsToDisplay.length < 4 ? "blog-grid-big" : ""}`}
+        >
+          {projectsToDisplay.map((item: Product, index: number) => {
+            return (
+              <ProductShelfItemComponent
+                key={index}
+                name={item.name}
+                link={item.link}
+                imageUrl={item.imageUrl}
+                supported={item.supported}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,65 @@
-import Script from 'next/script'
-import React from 'react'
+import Script from "next/script";
+import React from "react";
 
 import {
-	AthenaPromoComponent,
-	BlogComponent,
-	BookClubComponent,
-	FooterComponent,
-	ForBusinessComponent,
-	FormulaPromoComponent,
-	HeartthrobPromoComponent,
-	PageHeroComponent,
-	PodcastsComponent,
-	ProductShelfComponent,
-	ProfileComponent,
-	TilesComponent,
-	TwitchComponent,
-} from '../components'
-import { SeoProps } from '../database/SEOProps'
+  AthenaPromoComponent,
+  BlogComponent,
+  BookClubComponent,
+  FooterComponent,
+  ForBusinessComponent,
+  FormulaPromoComponent,
+  HeartthrobPromoComponent,
+  PageHeroComponent,
+  PodcastsComponent,
+  ProductShelfComponent,
+  ProfileComponent,
+  TilesComponent,
+  TwitchComponent,
+} from "../components";
+import { SeoProps } from "../database/SEOProps";
 
 export default function Index() {
-	return (
-		<>
-			<SeoProps title='vítor norton | developer relations | 🤘🚀🥑' description='Lives na Twitch, clube do livro, desenvolvimento de apps e web, mitologia grega, música e séries de TV. Um compilado do que eu sou e do que eu faço, chega mais!' featureImage='/img/pages/404.jpg' />
-			<PageHeroComponent title='vítor norton' description='Lives na Twitch, clube do livro, desenvolvimento de apps e web, mitologia grega, música e séries de TV. Um compilado do que eu sou e do que eu faço, chega mais!' backgroundUrl='/img/pages/404.jpg'>
-				<BlogComponent></BlogComponent>
-			</PageHeroComponent>
+  return (
+    <>
+      <SeoProps
+        title="vítor norton | developer relations | 🤘🚀🥑"
+        description="Lives na Twitch, clube do livro, desenvolvimento de apps e web, mitologia grega, música e séries de TV. Um compilado do que eu sou e do que eu faço, chega mais!"
+        featureImage="/img/pages/404.jpg"
+      />
+      <PageHeroComponent
+        title="vítor norton"
+        description="Lives na Twitch, clube do livro, desenvolvimento de apps e web, mitologia grega, música e séries de TV. Um compilado do que eu sou e do que eu faço, chega mais!"
+        backgroundUrl="/img/pages/404.jpg"
+      >
+        <BlogComponent></BlogComponent>
+      </PageHeroComponent>
 
-			
-			<div className='container'>
-				<div className='row'>
-					<div className='col-md-6'>
-						<TwitchComponent />
-						<TilesComponent />
-						{/* <CursoDevPromoComponent half /> */}
-						{/* <CursoTechPromoComponent half /> */}
-						<ForBusinessComponent />
-					</div>
-					<div className='col-md-6'>
-						<BookClubComponent />
-						<PodcastsComponent />
-					</div>
-				</div>
-			</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <TwitchComponent />
+            <TilesComponent />
+            {/* <CursoDevPromoComponent half /> */}
+            {/* <CursoTechPromoComponent half /> */}
+            <ForBusinessComponent />
+            <AthenaPromoComponent half></AthenaPromoComponent>
+          </div>
+          <div className="col-md-6">
+            <BookClubComponent />
+            <PodcastsComponent />
+          </div>
+        </div>
+      </div>
 
-			
+      <ProductShelfComponent showBusinessCases={false} />
 
-			<div className='container'>
-				<div className='row'>
-						<div className='col-md-6 col-sm-12'>
-							<FormulaPromoComponent half></FormulaPromoComponent>
-						</div>
-						<div className='col-md-6 col-sm-12'>
-							<AthenaPromoComponent half></AthenaPromoComponent>
-						</div>
-				</div>
-			</div>
-			
-			<ProductShelfComponent showBusinessCases={false} />
+      <div className="container">
+        <HeartthrobPromoComponent></HeartthrobPromoComponent>
+      </div>
 
-			<div className='container'>
-				<HeartthrobPromoComponent></HeartthrobPromoComponent>
-			</div>
+      <ProfileComponent />
 
-			<ProfileComponent />
-
-			<FooterComponent />
-		</>
-	)
+      <FooterComponent />
+    </>
+  );
 }
