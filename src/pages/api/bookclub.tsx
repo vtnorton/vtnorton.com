be05983291a2ca4198db96d19cd47318e34c6b36
Vitor Norton <vtnorton@outlook.com>
2024-05-26 11:34:43 -0300

    book club

--- src/pages/api/bookclub.tsx
+++ src/pages/api/bookclub.tsx
@@ -5,7 +5,7 @@ import { CalendarItem } from '../../interfaces'
 import { getBoraLerEvents } from '../../services/calendarServices'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<CalendarItem[]>) {
-	const cacheKey = 'bookclub'
+	const cacheKey = 'boraler'
 	const cachedEvents = (await kv.get(cacheKey)) as CalendarItem[]
 
 	if (cachedEvents) return res.status(200).json(cachedEvents)
