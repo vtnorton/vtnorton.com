42dd3d8c033f18f62cbd81d4eb39b5b1b24503af
Vitor Norton <vtnorton@outlook.com>
2024-12-01 17:26:43 -0300

    Fixed lint

--- src/components/SectionComponent/Section.tsx
+++ src/components/SectionComponent/Section.tsx
@@ -29,7 +29,7 @@ export const Section = ({ children, full, half, className, removePaddingButton, 
 				<div className='section-image'>
 					<div className='section-image-blur' style={{
 						backgroundColor: image.solidColor ? image.solidColor : 'transparent',
-						backgroundImage: image.imageBlurURL ? 'url(' + image.imageBlurURL + ')' : 'none'
+						backgroundImage: image.imageBlurURL ? 'url(' + image.imageBlurURL + ')' : 'none',
 					}}></div>
 					<img src={image.imageURL} alt={image.imageAlt} />
 				</div>
