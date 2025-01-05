c5dbb7c8746687fa808ce254fa57956ce57a5a98
Vitor Norton <vtnorton@outlook.com>
2025-01-05 11:21:59 -0300

    Refactor post filter logic

--- src/services/postsServices.tsx
+++ src/services/postsServices.tsx
@@ -1,18 +1,10 @@
 import { Post } from '../models/Post'
 import { NotionDatabaseAdapter } from './adapter/notionDatabaseAdapter'
 
-export const getPosts = async (tag?: string): Promise<Post[]> => {
-	const idCliente = process.env.CLIENT_ID
-	const NOTION_DB_DEVREL = process.env.DEVREL_DB as string
-	const notion = new NotionDatabaseAdapter(NOTION_DB_DEVREL)
+const sharedFilter = (): any => {
+	const CLIENT_ID = process.env.CLIENT_ID
 
-	const filter = [
-		{
-			property: 'Type',
-			multi_select: {
-				contains: 'Post',
-			},
-		},
+	return [
 		{
 			property: 'Date',
 			date: {
@@ -42,7 +34,7 @@ export const getPosts = async (tag?: string): Promise<Post[]> => {
 			],
 		},
 		{
-			or: [ // TODO: passar para o queryNotion
+			or: [
 				{
 					property: 'Cliente',
 					relation: {
@@ -52,11 +44,31 @@ export const getPosts = async (tag?: string): Promise<Post[]> => {
 				{
 					property: 'Cliente',
 					relation: {
-						contains: idCliente,
+						contains: CLIENT_ID,
 					},
 				},
 			],
 		},
+	]
+}
+
+const itemTypeFilter = (type: string): any => {
+	return {
+		property: 'Type',
+		multi_select: {
+			contains: type,
+		},
+	}
+}
+
+export const getPosts = async (tag?: string): Promise<Post[]> => {
+	const NOTION_DB_DEVREL = process.env.DEVREL_DB as string
+
+	const notion = new NotionDatabaseAdapter(NOTION_DB_DEVREL)
+
+	const filter = [
+		...sharedFilter(),
+		itemTypeFilter('Post'),
 	]
 
 	if (tag) {
@@ -71,6 +83,5 @@ export const getPosts = async (tag?: string): Promise<Post[]> => {
 	const results = await notion.query(filter)
 
 	const posts = results.map((result: any) => new Post(result))
-
 	return posts.filter((post): post is Post => typeof post !== 'undefined')
 }
