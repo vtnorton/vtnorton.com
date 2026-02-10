1d98c10cf99c23bffc5ac85a30acc47637d2bc48
Vitor Norton <vitor@vtnorton.com>
2026-02-10 11:15:47 -0300

    feat: update heading in PodcastsContent for clarity

--- src/modules/Portfolio/Numbers/Content/PodcastsContent.tsx
+++ src/modules/Portfolio/Numbers/Content/PodcastsContent.tsx
@@ -1,7 +1,7 @@
 export const PodcastsContent = () => {
 	return (
 		<div className='expanded-content'>
-			<h3>Construindo...</h3>
+			<h3>Construindo essa parte...</h3>
 			<p>Este site está tendo, em média, 5 atualizações por dia. Realmente, em breve!</p>
 		</div>
 	)
