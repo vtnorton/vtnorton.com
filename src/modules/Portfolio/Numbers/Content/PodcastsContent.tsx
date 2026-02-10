2204a1e0996031814c71983dc16cdb655120cef4
Vitor Norton <vitor@vtnorton.com>
2026-02-10 11:17:20 -0300

    feat: enhance PodcastsContent with link for update statistics and style links in numbers.scss

--- src/modules/Portfolio/Numbers/Content/PodcastsContent.tsx
+++ src/modules/Portfolio/Numbers/Content/PodcastsContent.tsx
@@ -2,7 +2,7 @@ export const PodcastsContent = () => {
 	return (
 		<div className='expanded-content'>
 			<h3>Construindo essa parte...</h3>
-			<p>Este site está tendo, em média, 5 atualizações por dia. Realmente, em breve!</p>
+			<p>Este site está tendo, em média, <a href='https://github.com/vtnorton/vtnorton.com/commits/main/' target='_blank' rel='noopener noreferrer'>5 atualizações por dia</a>. Realmente, em breve!</p>
 		</div>
 	)
 }
