62e2956509463925c79c5a0caab7a8d8e916464a
Vitor Norton <vitor@vtnorton.com>
2026-02-19 15:44:02 -0300

    feat: widket na pagina de palestra

--- src/components/PalestraComponent/PalestraFeed.tsx
+++ src/components/PalestraComponent/PalestraFeed.tsx
@@ -1,9 +1,11 @@
 import { useCallback, useEffect, useState } from 'react'
-import { Talk } from '../../models/Talk'
+import { Talk, TalkStatus } from '../../models/Talk'
 import { PostFeedItemLoader } from '../../modules/Blog/Feed/PostFeedItemLoader'
 import { PalestraItem } from './PalestraItem'
+import { Button } from '../Button'
 export const PalestraFeed = () => {
 	const [talks, setTalks] = useState<Talk[]>([])
+	const [talksToPresent, setTalksToPresent] = useState<Talk[]>([])
 	const [loading, setLoading] = useState(false)
 
 	const loadPosts = useCallback(async () => {
@@ -17,7 +19,12 @@ export const PalestraFeed = () => {
 
 			const data = await response.json() as Talk[]
 
-			setTalks((prevTalks) => [...prevTalks, ...data])
+			const presentedTalks = data.filter((talk) => talk.status === TalkStatus.Presented)
+			const soonTMTalks = data.filter((talk) => talk.status === TalkStatus.SoonTM)
+			const call4PappersTalks = data.filter((talk) => talk.status === TalkStatus.Call4Pappers)
+
+			setTalksToPresent(soonTMTalks)
+			setTalks([presentedTalks, call4PappersTalks].flat())
 		} catch (error) {
 			console.error('Erro ao obter os dados da API:', error)
 		} finally {
@@ -26,8 +33,6 @@ export const PalestraFeed = () => {
 	}, [])
 
 	useEffect(() => {
-		// if (!isReady) return
-
 		setTalks([])
 		loadPosts()
 	}, [loadPosts])
@@ -48,6 +53,26 @@ export const PalestraFeed = () => {
 					<PostFeedItemLoader />
 				</>}
 			</div>
+			<div>
+				{talksToPresent.length > 0 && (
+					<div className='widgets'>
+						<div className='to-present-talk'>
+							<h3>Palestras que quero fazer acontecer</h3>
+							<br />
+							<small>Tenho vontade de apresentar estes temas, esperando o convite certo. Se alguma te interessar, me chama. Se quiser um tema específico, estou aberto a sugestões também, mas preciso tirar essas do meu to-do list antes que elas virem promessas que nunca saem do papel.</small>
+							<br /><br />
+							<ul>
+								{talksToPresent.map((talk) => (
+									<li key={`soon-tm-talk-${talk.id}`}>/{talk.title}</li>
+								))}
+							</ul>
+							<br />
+							<Button as='a' href='mailto:vitor@vtnorton.com' size='small' appearance='outline' shape='square'>Me chame para palestrar</Button>
+						</div>
+					</div>
+				)}
+			</div>
+
 		</>
 	)
 }
