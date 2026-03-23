8af2bfff0947c87c00cb858ca61156d964d48f05
Vitor Norton <vitor@vtnorton.com>
2026-03-23 17:47:23 -0300

    refactor: streamline data fetching in PostDetail component

--- src/pages/[year]/[month]/[slug].tsx
+++ src/pages/[year]/[month]/[slug].tsx
@@ -1,3 +1,4 @@
+import type { GetServerSidePropsContext } from 'next'
 import { Post } from '../../../models/Post'
 import { IndividualPost } from '../../../modules/Blog'
 import { postServices } from '../../../services/postsServices'
@@ -5,54 +6,30 @@ import { useLayout } from '../../../providers/LayoutProvider'
 import { useLayoutEffect } from 'react'
 import { ContentSEO } from '../../../database/seo'
 
-const mountPath = (post: Post) => {
-	const postDate = new Date(post.date)
-	const monthNumber = postDate.getMonth() + 1
-	const month = monthNumber.toString().padStart(2, '0')
-
-	return {
-		params: {
-			slug: post.slug.split('/').pop() || post.slug,
-			year: postDate.getFullYear().toString(),
-			month: month,
-		},
-	}
-}
-
-export const getStaticPaths = async () => {
-	const posts = await postServices.getPosts()
-	return {
-		paths: posts.map((post: Post) => mountPath(post)),
-		fallback: 'blocking',
-	}
-}
-
-export const getStaticProps = async (
-	context: {
-		params: {
-			slug: string
-			year: string
-			month: string
-		}
-	},
-) => {
-	const { slug, year, month } = context.params
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+	const { slug, year, month } = context.params as { slug: string; year: string; month: string }
 
 	const post = await postServices.getPostBySlug(`/${year}/${month}/${slug}`)
-	const relatedPosts = post
-		? await postServices.getRelatedPosts(post, 3)
-		: []
+
+	if (!post) {
+		return { notFound: true }
+	}
+
+	const relatedPosts = await postServices.getRelatedPosts(post, 3)
 
 	let props = {
-		post: post,
+		post,
 		relatedPosts,
 	}
 
 	props = JSON.parse(JSON.stringify(props))
-	return {
-		props,
-		revalidate: 60 * 60 * 1,
-	}
+
+	context.res.setHeader(
+		'Cache-Control',
+		'public, s-maxage=3600, stale-while-revalidate=59',
+	)
+
+	return { props }
 }
 
 export default function PostDetail({ post, relatedPosts }: { post: Post; relatedPosts: Post[] }) {
@@ -69,9 +46,6 @@ export default function PostDetail({ post, relatedPosts }: { post: Post; related
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
-
-	if (!post)
-		return <div />
 
 	return (
 		<>
